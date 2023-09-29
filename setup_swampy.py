efbd732a12eb458ab7c8d88b79d0188d4d28fca4
Kyle Dennis Murray <kylemurray2@gmail.com>
2023-09-29 12:08:45 -1000

    update

---
+++ setup_swampy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os,shutil
+from swampy import download_dsw
+
+swampyPath = download_dsw.__file__
+swampyPath = os.path.dirname(swampyPath)
+
+# If you want to ensure the output always ends with a '/'
+paramPath = os.path.join(swampyPath, 'docs', 'params_template.yaml')
+
+print('Copying from ' + paramPath)
+
+shutil.copy(paramPath, './params.yaml')
+
+# Define the path to your file
+file_path = os.path.join(swampyPath, 'docs', 'swampy.txt')
+
+# Open the file and print its contents
+with open(file_path, 'r') as file:
+    contents = file.read()
+    print(contents)
+
+print('\n')
+print('Copying the params.yaml template.')
+print('Next, edit the params.yaml file in this directory. Then you can download data with download_dsw.py')
+print('\n')
